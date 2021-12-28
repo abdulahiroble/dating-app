ef39d1ad723e9dff1f7ca985880f9f0a86f026cf
Thomas WEst <thom8750@stud.kea.dk>
2021-12-28 14:23:17 +0100

    fields added to xml

--- app/src/main/java/com/example/gamingnews/MainActivity.java
+++ app/src/main/java/com/example/gamingnews/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
